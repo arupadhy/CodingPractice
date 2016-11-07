0f4314c86adb2835e6ea4cd48dfac89fd504a795
arvind upadhyay <arvind.charlotte@gmail.com>
2016-11-07 17:21:52 -0500

    converting to maven project to write test cases and showcase TDD

--- HackerRank/main/java/com/arvind/hackerrank/NumberOfBST.java
+++ HackerRank/main/java/com/arvind/hackerrank/NumberOfBST.java
@@ -6,12 +6,12 @@ package com.arvind.hackerrank;
  *
  */
 public class NumberOfBST {
-	
-	private static int numberOfBST(int keys) {
+
+	public  int numberOfBST(int keys) {
 		if(keys == 0) {
 			return 1;
 		}
-	else if(keys == 1) {
+		else if(keys == 1) {
 			return 1;
 		}else if(keys == 2) {
 			return 2;
